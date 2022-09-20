db6486bc6f9fa2afe89000f0802291c381a9fca7
mt <tavgen.m@yandex.ru>
2022-09-20 18:35:38 +0300

    stats.txt added

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # reorganize-string
-leetcode 767. Reorganize String with benchmarks
+Leetcode 767. Reorganize String with benchmarks
+
+Run
+
+``GOGC=off go test -bench=. -benchtime=100000x -benchmem | tee stats.txt``
+
+Check out stats.txt
--- bench/bench_test.go
+++ bench/bench_test.go
@@ -5,22 +5,28 @@ import (
 )
 
 func BenchmarkReorganizeString(b *testing.B) {
+	var r string
+
 	for i := 0; i < b.N; i++ {
-		r := ReorganizeString(QueryString)
-		_ = r
+		r = ReorganizeString(QueryString)
 	}
+	_ = r
 }
 
 func BenchmarkReorganizeStringSlice(b *testing.B) {
+	var r string
+
 	for i := 0; i < b.N; i++ {
-		r := ReorganizeStringSlice(QueryString)
-		_ = r
+		r = ReorganizeStringSlice(QueryString)
 	}
+	_ = r
 }
 
 func BenchmarkReorganizeStringVer3(b *testing.B) {
+	var r string
+
 	for i := 0; i < b.N; i++ {
-		r := reorganizeStringVer3(QueryString)
-		_ = r
+		r = reorganizeStringVer3(QueryString)
 	}
+	_ = r
 }
